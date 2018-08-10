09244fcf97b35d400da92ca1b02adec835246a80
Alexey Melezhik <melezhik@gmail.com>
2018-08-10 16:35:10 +0000

    ionic build instead of cordova

--- resources/windows/cordova-build-windows.cmd
+++ resources/windows/cordova-build-windows.cmd
@@ -1,5 +1,5 @@
 set PATH=%PATH%;"C:\Program Files\nodejs"
-set  VSINSTALLDIR=C:\Program Files (x86)\Microsoft Visual Studio\2017\Professional
+REM set  VSINSTALLDIR=C:\Program Files (x86)\Microsoft Visual Studio\2017\Professional
 set MSBUILDDIR=C:\Program Files (x86)\Microsoft Visual Studio\2017\Professional\MSBuild\15.0\Bin
-node_modules\.bin\cordova.cmd build [% build_configuration %] windows --verbose -- [% ms_build_opts %]
-
+REM node_modules\.bin\cordova.cmd build [% build_configuration %] windows --verbose -- [% ms_build_opts %]
+npm run ionic -- cordova build windows  -- --arch="x86"
--- resources/windows/cordova-platorm-add-windows.cmd
+++ resources/windows/cordova-platorm-add-windows.cmd
@@ -1,8 +1,3 @@
 set PATH=%PATH%;"C:\Program Files\nodejs"
 
-IF EXIST platforms\windows\CordovaApp.sln (
-echo 'platforms\windows\CordovaApp.sln exist - skip this step'
-) ELSE (
-node_modules\.bin\cordova.cmd platform add windows
-)
-
+npm run ionic -- build 

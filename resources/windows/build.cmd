3f71b12642a3721e5aa95ef7f1c89701a6688b30
Alexey Melezhik <melezhik@gmail.com>
2018-08-13 15:51:54 +0000

    double dash is important

--- resources/windows/build.cmd
+++ resources/windows/build.cmd
@@ -1,4 +1,4 @@
 set PATH=%PATH%;"C:\Program Files\nodejs"
-REM set  VSINSTALLDIR=C:\Program Files (x86)\Microsoft Visual Studio\2017\Professional
+set VSINSTALLDIR=C:\Program Files (x86)\Microsoft Visual Studio\2017\Professional
 set MSBUILDDIR=C:\Program Files (x86)\Microsoft Visual Studio\2017\Professional\MSBuild\15.0\Bin
 npm run ionic -- cordova build windows --[% build_configuration %] --arch=[% build_arch %]

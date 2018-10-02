20075a254a38bd00bc7db94b226fdebe401d9ff4
Alexey Melezhik <melezhik@gmail.com>
2018-10-02 17:14:58 +0000

    small refactoring

--- resources/windows/remove-old-packages.pl
+++ resources/windows/remove-old-packages.pl
@@ -6,11 +6,15 @@ my $path = $ARGV[0] or die "usage: remove-old-packages.pl path";
 if (-d $path){
   remove_tree($path) or die "can't remove directory $path, error: $!";
   print "directory [$path] removed OK\n";
+} else {
+  print "directory [$path] does not exit, SKIP removal\n";
 }
 
 if (-d "binaries"){
   remove_tree("binaries") or die "can't remove directory binaries, error: $!";
   print "directory [binaries] removed OK\n";
+} else {
+  print "directory [binaries] does not exit, SKIP removal\n";
 }
 
 make_path("binaries") or die "can't create directory binaries, error: $!";

f118eb3f8215587190a21ceb7bafea4f5601ac84
Alexey Melezhik <melezhik@gmail.com>
2018-10-02 17:30:31 +0000

    small refactoring

--- resources/windows/copy-appx-files.pl
+++ resources/windows/copy-appx-files.pl
@@ -1,32 +1,46 @@
 use strict;
 use File::Copy;
+use JSON qw{decode_json};
 
 
-my $build_version = @ARGV[0] or die "usage: copy-appx-files.pl build_version build_configuration build_arch";
+my $build_id = @ARGV[0] or die "usage: copy-appx-files.pl build_id build_configuration build_arch";
 
-my $build_configuration = @ARGV[1] or die "usage: copy-appx-files.pl build_version build_configuration build_arch";
+my $build_configuration = @ARGV[1] or die "usage: copy-appx-files.pl build_version build_id build_arch";
 
-my $build_arch = @ARGV[2] or die "usage: copy-appx-files.pl build_version build_configuration build_arch";
+my $build_arch = @ARGV[2] or die "usage: copy-appx-files.pl build_version build_id build_arch";
 
 # see https://stackoverflow.com/questions/1188284/net-large-revision-numbers-in-assemblyversionattribute
 # why
 
-$build_version = $build_version - 35365 * (sprintf "%d",  $build_version / 35365);
+my $revision = $build_id - 35365 * (sprintf "%d",  $build_id / 35365);
 
+
+open JSON, "package.json" or die "can't open package.jspon to read";
+my $js = join "", <JSON>;
+close JSON;
+
+my $main_version = decode_json($js)->{version};
+
+print "copy appx file ...\n";
+print "main version (taken from package.json) - $main_version ...\n";
+print "build_id - $build_id ...\n";
+print "revision version (calculated by build_id) - $revision ...\n";
+print "arch - $build_arch ...\n";
+print "configuration - $build_configuration ...\n";
 
 if ( $build_configuration eq 'debug' ){
 
-  my $path = "platforms/windows/AppPackages/CordovaApp.Windows10_0.1.0.".
-  $build_version.
-  "_".$build_arch."_".$build_configuration."_Test/CordovaApp.Windows10_0.1.0.".
-  $build_version."_".$build_arch."_".$build_configuration.".appx";
+  my $path = "platforms/windows/AppPackages/CordovaApp.Windows10_$main_version.".
+  $revision.
+  "_".$build_arch."_".$build_configuration."_Test/CordovaApp.Windows10_$main_version.".
+  $revision."_".$build_arch."_".$build_configuration.".appx";
 
   print "copy $path => binaries/ ... \n";
   copy("$path","binaries/$path") or die "Copy failed: $!";
 
-  $path = "platforms/windows/AppPackages/CordovaApp.Windows10_0.1.0.".
-  $build_version."_".$build_arch."_".$build_configuration."_Test/CordovaApp.Windows10_0.1.0.".
-  $build_version."_".$build_arch."_".$build_configuration.".appxsym";
+  $path = "platforms/windows/AppPackages/CordovaApp.Windows10_$main_version.".
+  $revision."_".$build_arch."_".$build_configuration."_Test/CordovaApp.Windows10_$main_version.".
+  $revision."_".$build_arch."_".$build_configuration.".appxsym";
 
   print "copy $path => binaries/ ... \n";
   copy("$path","binaries/$path") or die "Copy failed: $!";
@@ -34,16 +48,16 @@ if ( $build_configuration eq 'debug' ){
 
 if ( $build_configuration eq 'release' ){
 
-  my $path = "platforms/windows/AppPackages/CordovaApp.Windows10_0.1.0.".
-  $build_version."_".$build_arch."_Test/CordovaApp.Windows10_0.1.0.".
-  $build_version."_".$build_arch.".appx";
+  my $path = "platforms/windows/AppPackages/CordovaApp.Windows10_$main_version.".
+  $revision."_".$build_arch."_Test/CordovaApp.Windows10_$main_version.".
+  $revision."_".$build_arch.".appx";
 
   print "copy $path => binaries/ ... \n";
   copy("$path","binaries/$path") or die "Copy failed: $!";
 
-  $path = "platforms/windows/AppPackages/CordovaApp.Windows10_0.1.0.".
-  $build_version."_".$build_arch."_Test/CordovaApp.Windows10_0.1.0.".
-  $build_version."_".$build_arch.".appxsym";
+  $path = "platforms/windows/AppPackages/CordovaApp.Windows10_$main_version.".
+  $revision."_".$build_arch."_Test/CordovaApp.Windows10_$main_version.".
+  $revision."_".$build_arch.".appxsym";
 
   print "copy $path => binaries/ ... \n";
   copy("$path","binaries/$path") or die "Copy failed: $!";
--- resources/windows/set-version.pl
+++ resources/windows/set-version.pl
@@ -4,20 +4,25 @@ use strict;
 
 use JSON qw{decode_json};
 
-my $build_version = @ARGV[0];
+my $build_id = @ARGV[0];
 
 # see https://stackoverflow.com/questions/1188284/net-large-revision-numbers-in-assemblyversionattribute
 # why
 
-$build_version = $build_version - 35365 * (sprintf "%d",  $build_version / 35365);
+my $revision = $build_id - 35365 * (sprintf "%d",  $build_id / 35365);
 
 open JSON, "package.json" or die "can't open package.jspon to read";
 my $js = join "", <JSON>;
 close JSON;
 
-my $version = decode_json($js)->{ version};
+my $main_version = decode_json($js)->{version};
 
-system("npm run cordova-set-version -- -v $version.$build_version")  == 0 
-  or die "npm run cordova-set-version -- -v $version.$build_version failed: $?";
+print "set version ...\n";
+print "main version (taken from package.json) - $main_version ...\n";
+print "build_id - $build_id ...\n";
+print "revision version (calculated by build_id) - $revision ...\n";
+
+system("npm run cordova-set-version -- -v $main_version.$revision")  == 0 
+  or die "npm run cordova-set-version -- -v $main_version.$revision failed: $?";
 
 

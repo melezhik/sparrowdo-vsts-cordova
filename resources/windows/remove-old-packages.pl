246a3cb8dba04ba04003e29110ec99fa62a4d171
Alexey Melezhik <melezhik@gmail.com>
2018-10-02 15:55:45 +0000

    set version, address Large revision numbers

--- resources/windows/remove-old-packages.pl
+++ resources/windows/remove-old-packages.pl
@@ -5,5 +5,6 @@ my $path = $ARGV[0] or die "usage: remove-old-packages.pl path";
 
 if (-d $path){
   remove_tree($path) or die "can't remove direrctory $path, error: $!";
+  print "directory [$path] removed OK\n";
 }
 
--- resources/windows/set-version.pl
+++ resources/windows/set-version.pl
@@ -6,6 +6,11 @@ use JSON qw{decode_json};
 
 my $build_version = @ARGV[0];
 
+# see https://stackoverflow.com/questions/1188284/net-large-revision-numbers-in-assemblyversionattribute
+# why
+
+$build_version = $build_version - 35365 * (sprintf "%d",  $build_version / 35365);
+
 open JSON, "package.json" or die "can't open package.jspon to read";
 my $js = join "", <JSON>;
 close JSON;

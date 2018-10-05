c2dbe13a8e3b43aaabae3196115b27b8aedab8df
Alexey Melezhik <melezhik@gmail.com>
2018-10-05 14:34:09 +0000

    execute commands

--- lib/Sparrowdo/VSTS/YAML/Cordova.pm6
+++ lib/Sparrowdo/VSTS/YAML/Cordova.pm6
@@ -26,7 +26,6 @@ our sub tasks (%args) {
     set-version.pl
     remove-old-packages.pl
     update-configuration.pl
-    configure.pl
     prepare.cmd
     build.json
   >;
--- resources/windows/update-configuration.pl
+++ resources/windows/update-configuration.pl
@@ -5,10 +5,9 @@ use File::Copy;
 
 my $source_code_branch = @ARGV[0];
 
+print "update configuration for env: $source_code_branch (data)\n=================================\n";
 
-if ( $source_code_branch && -d "src/env/$source_code_branch/" ) {
-
-  print "update configuration for env: $source_code_branch\n=================================\n";
+if ( $source_code_branch && -d "src/env/$source_code_branch/" && -d "src/assets/jsons/" ) {
 
   opendir(my $dh, "src/env/$source_code_branch/" ) || die "Can't open directory src/env/$source_code_branch/ to read: $!";
   while ( my $i = readdir $dh) {
@@ -20,7 +19,30 @@ if ( $source_code_branch && -d "src/env/$source_code_branch/" ) {
   closedir $dh;
 } elsif ( ! $source_code_branch ) {
   warn 'source branch is not set, is Build.SourceBranchName empty? .... nothing to do'
+} elsif ( ! -d "src/assets/jsons/" ) {
+  warn "src/assets/jsons/ does not exit, hope it's ok ... nothing to do"
 } else {
   warn "directory src/env/$source_code_branch/ does not exit, hope it's ok ... nothing to do"
+}
+
+print "update configuration for env: $source_code_branch (cmd)\n=================================\n";
+
+if ( $source_code_branch && -d "src/env/$source_code_branch/" ) {
+  my @commands;
+  opendir(my $dh, "src/env/$source_code_branch/" ) || die "Can't open directory src/env/$source_code_branch/ to read: $!";
+  while ( my $i = readdir $dh) {
+    $i =~ /.*\.cmd/ or next;
+    -f "src/env/$source_code_branch/$i" or next;
+    push @commands, $i;
+  }
+  closedir $dh;
+
+  for my $c (sort { $a <=> $b } @commands){
+    print "executing src/env/$source_code_branch/$c ... \n";
+    system("src/env/$source_code_branch/$c") == 0 or die "Failed to execute [src/env/$source_code_branch/$c]: $!";
+  }
+
+} else {
+  warn 'source branch is not set, is Build.SourceBranchName empty? .... nothing to do'
 }
 

2a27d4c62673a6fa1b53318a62f9500cd695a356
Alexey Melezhik <melezhik@gmail.com>
2018-10-05 16:00:07 +0000

    small fixes for configuration script

--- resources/windows/update-configuration.pl
+++ resources/windows/update-configuration.pl
@@ -8,6 +8,29 @@ my $source_code_branch = @ARGV[0];
 print "update configuration for env: $source_code_branch (data)\n=================================\n";
 
 if ( $source_code_branch && -d "src/env/$source_code_branch/" && -d "src/assets/jsons/" ) {
+  copy_data($source_code_branch);
+} elsif ( ! $source_code_branch ) {
+  print "source branch is not set, is Build.SourceBranchName empty? .... nothing to do\n";
+} elsif ( ! -d "src/assets/jsons/" ) {
+  print "src/assets/jsons/ does not exit, hope it's ok ... nothing to do\n";
+} else {
+  copy_data("default");
+}
+
+print "update configuration for env: $source_code_branch (cmd)\n=================================\n";
+
+if ( $source_code_branch && -d "src/env/$source_code_branch/" ) {
+  execute_commands($source_code_branch);
+} elsif ( ! $source_code_branch ) {
+  print "source branch is not set, is Build.SourceBranchName empty? .... nothing to do\n";
+} else {
+  execute_commands("default");
+}
+
+
+sub copy_data {
+
+  my $source_code_branch = shift;
 
   opendir(my $dh, "src/env/$source_code_branch/" ) || die "Can't open directory src/env/$source_code_branch/ to read: $!";
   while ( my $i = readdir $dh) {
@@ -17,24 +40,24 @@ if ( $source_code_branch && -d "src/env/$source_code_branch/" && -d "src/assets/
     copy("src/env/$source_code_branch/$i","src/assets/jsons/$i") or die "Copy failed: $!";
   }
   closedir $dh;
-} elsif ( ! $source_code_branch ) {
-  warn 'source branch is not set, is Build.SourceBranchName empty? .... nothing to do'
-} elsif ( ! -d "src/assets/jsons/" ) {
-  warn "src/assets/jsons/ does not exit, hope it's ok ... nothing to do"
-} else {
-  warn "directory src/env/$source_code_branch/ does not exit, hope it's ok ... nothing to do"
+
 }
 
-print "update configuration for env: $source_code_branch (cmd)\n=================================\n";
 
-if ( $source_code_branch && -d "src/env/$source_code_branch/" ) {
+sub execute_command {
+
+  my $source_code_branch = shift;
+
   my @commands;
+
   opendir(my $dh, "src/env/$source_code_branch/" ) || die "Can't open directory src/env/$source_code_branch/ to read: $!";
+
   while ( my $i = readdir $dh) {
     $i =~ /.*\.cmd/ or next;
     -f "src/env/$source_code_branch/$i" or next;
     push @commands, $i;
   }
+
   closedir $dh;
 
   for my $c (sort { $a <=> $b } @commands){
@@ -42,7 +65,5 @@ if ( $source_code_branch && -d "src/env/$source_code_branch/" ) {
     system("src/env/$source_code_branch/$c") == 0 or die "Failed to execute [src/env/$source_code_branch/$c]: $!";
   }
 
-} else {
-  warn 'source branch is not set, is Build.SourceBranchName empty? .... nothing to do'
 }
 

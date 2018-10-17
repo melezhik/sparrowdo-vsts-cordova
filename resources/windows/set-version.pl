fd73902c457075940869bdd68871af4496879c90
Alexey Melezhik <melezhik@gmail.com>
2018-10-17 20:19:52 +0000

    skip version patch conditionally

--- resources/windows/set-version.pl
+++ resources/windows/set-version.pl
@@ -2,27 +2,33 @@
 
 use strict;
 
-use JSON qw{decode_json};
+if ($ENV{BUILD_NOPATCHVERSION}){
 
-my $build_id = @ARGV[0];
+  print "Build.NoPatchVersion is set ... nothing to do ... \n";
 
-# see https://stackoverflow.com/questions/1188284/net-large-revision-numbers-in-assemblyversionattribute
-# why
+} else {
 
-my $revision = $build_id - 35365 * (sprintf "%d",  $build_id / 35365);
-
-open JSON, "package.json" or die "can't open package.jspon to read";
-my $js = join "", <JSON>;
-close JSON;
-
-my $main_version = decode_json($js)->{version};
-
-print "set version ...\n";
-print "main version (taken from package.json) - $main_version ...\n";
-print "build_id - $build_id ...\n";
-print "revision version (calculated by build_id) - $revision ...\n";
-
-system("npm run cordova-set-version -- -v $main_version.$revision")  == 0 
-  or die "npm run cordova-set-version -- -v $main_version.$revision failed: $?";
-
+  use JSON qw{decode_json};
+  
+  my $build_id = @ARGV[0];
+  
+  # see https://stackoverflow.com/questions/1188284/net-large-revision-numbers-in-assemblyversionattribute
+  # why
+  
+  my $revision = $build_id - 35365 * (sprintf "%d",  $build_id / 35365);
+  
+  open JSON, "package.json" or die "can't open package.jspon to read";
+  my $js = join "", <JSON>;
+  close JSON;
+  
+  my $main_version = decode_json($js)->{version};
+  
+  print "set version ...\n";
+  print "main version (taken from package.json) - $main_version ...\n";
+  print "build_id - $build_id ...\n";
+  print "revision version (calculated by build_id) - $revision ...\n";
+  
+  system("npm run cordova-set-version -- -v $main_version.$revision")  == 0 
+    or die "npm run cordova-set-version -- -v $main_version.$revision failed: $?";
+}
 
